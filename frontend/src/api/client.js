ce1d00bca643333524e27a06a6661bbcd83179bf
Thinesh Ananth <thineshananth12@gmail.com>
2026-05-14 23:31:08 +0530

    Front end url added from .env

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://timelog-api.local/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     Accept: 'application/json',
   },
